6b2106c681077ddb2dd486cd7fd523b564016669
Angel <ayog95@gmail.com>
2023-01-29 22:43:37 -0600

    Prueba P5

--- src/pages/Career/Career.jsx
+++ src/pages/Career/Career.jsx
@@ -1,5 +1,6 @@
 import "./Career.css";
 import { useMediaQuery } from "react-responsive";
+//import data from "./Career-info.json";
 
 const Career = () => {
 
@@ -10,19 +11,19 @@ const Career = () => {
   return (
     <div className="Career">
       <div className="Career-Header" style={ isMobile ? { fontSize : 40 } : { fontSize : 24 }  }>Aqui puedes ver mi carrera profesional!</div>
-      <div className="Career-List">
-        {/* { 
+      {/* <div className="Career-List">
+        { 
           keys.map((key)=>(
-            <Hobbie
-              key={key}
-              src={data[key]["src"]}
-              title={data[key]["titulo"]}
-              text={data[key]["texto"]}
-              align={data[key]["align"]}
-            />
+            <p className="testing" key={key}>
+              {
+                Object.keys(data[key]).map((key2)=>(
+                  <p className="data" key={key2}>{`${key2} : ${data[key][key2]}`}</p>
+                ))
+              }
+            </p>
           ))
-        } */}
-      </div>
+        }
+      </div> */}
     </div>
   );
 }
