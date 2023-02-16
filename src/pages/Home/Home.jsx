76fd52c44e8ff474ddfbae43b4428edd05411d3c
Angel <ayog95@gmail.com>
2023-02-15 21:05:43 -0600

    Adolecencia

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,10 @@
 import "./Home.css";
-import Sketch from "react-p5";
 
 const Home = () => {
 
-  let angle = Math.PI;
-
-  const setup = ( p5 , canvasParentRef ) => {
-    p5.createCanvas(200,200).parent(canvasParentRef);
-    p5.background("#C1CFC0");
-  };
-
-  const draw = ( p5 ) => {
-    p5.clear();
-    p5.ellipse(100,100, 150 + 25*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.sin(0.05*angle),50);
-    p5.fill(133,133,133);
-    p5.strokeWeight(2);
-    angle+=p5.deltaTime*0.1;
-  };
-
   return (
     <div className="Home">
-      <Sketch setup={setup} draw={draw} />
+      holi
     </div>
   );
 }
--- src/pages/p5/p5.jsx
+++ src/pages/p5/p5.jsx
@@ -1,12 +1,26 @@
 import "./p5.css";
+import Sketch from "react-p5";
 
 const P5 = () => {
 
+  let angle = Math.PI;
+
+  const setup = ( p5 , canvasParentRef ) => {
+    p5.createCanvas(200,200).parent(canvasParentRef);
+    p5.background("#C1CFC0");
+  };
+
+  const draw = ( p5 ) => {
+    p5.clear();
+    p5.ellipse(100,100, 150 + 25*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.sin(0.05*angle),50);
+    p5.fill(133,133,133);
+    p5.strokeWeight(2);
+    angle+=p5.deltaTime*0.1;
+  };
+
   return (
     <div className="p5">
-      <div className="p5-Header">
-        Aqui va el projecto realizado con p5
-      </div>
+      <Sketch setup={setup} draw={draw} />
     </div>
   );
 }
