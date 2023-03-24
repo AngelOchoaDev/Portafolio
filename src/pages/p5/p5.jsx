cac580331a2f4ef44ff4b5176598b0cb494f43e0
Angel <ayog95@gmail.com>
2023-03-24 17:02:24 -0600

    Avance semanal

--- src/pages/p5/p5.jsx
+++ src/pages/p5/p5.jsx
@@ -6,16 +6,16 @@ const P5 = () => {
   let angle = Math.PI;
 
   const setup = ( p5 , canvasParentRef ) => {
-    p5.createCanvas(200,200).parent(canvasParentRef);
+    p5.createCanvas(300,300).parent(canvasParentRef);
     p5.background("#C1CFC0");
   };
 
   const draw = ( p5 ) => {
     p5.clear();
-    p5.ellipse(100,100 + 30*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.sin(0.05*angle),50);
+    p5.ellipse(130,150 + 30*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.sin(0.05*angle),50);
     p5.fill(133,133,133);
     p5.strokeWeight(2);
-    p5.ellipse(150,100  + 30*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.sin(0.05*angle),50);
+    p5.ellipse(180,150  + 30*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.cos(0.05*angle + Math.PI/2), 150 + 25*Math.sin(0.05*angle),50);
     p5.fill(133,133,133);
     p5.strokeWeight(2);
     angle+=p5.deltaTime*0.1;
@@ -23,7 +23,7 @@ const P5 = () => {
 
   return (
     <div className="p5">
-      <div className="p5-Container">
+      <div className="p5-Container testing">
         <Sketch setup={setup} draw={draw} />
       </div>
     </div>
