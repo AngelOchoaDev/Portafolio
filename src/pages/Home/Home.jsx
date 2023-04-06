0033e47d135abc252682570f4d0ffed447064e86
Angel <ayog95@gmail.com>
2023-04-06 00:24:06 -0600

    Cambios 6 de abril

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,10 @@ const Home = () => {
 
   return (
     <div className="Home">
-      <div className="Home-Header">{ "Bienvenidos a mi pagina web !" }</div>
+      <div className="Home-Header">{ "¡ Bienvenidos a mi pagina web !" }</div>
+      <div className="Quien-soy">{ "¿Quién soy?" }</div>
+      <div className="A-Que-Me-Quiero-Dedicar">{ "¿A qué me quiero dedicar?" }</div>
+      <div className="Que-Me-Gusta-Hacer">{ "¿Qué me gusta hacer?" }</div>
     </div>
   );
 }
